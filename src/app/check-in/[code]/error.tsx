6808e76059abd1bdf805e31c41d5a708abe5e663
yuankunluo <yuankunluo@outlook.com>
2023-06-15 21:14:15 +0800

    error component

--- src/app/check-in/[code]/error.tsx
+++ src/app/check-in/[code]/error.tsx
@@ -1,5 +1,6 @@
 'use client' // Error components must be Client Components
  
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
  
 export default function Error({
@@ -9,18 +10,17 @@ export default function Error({
   error: Error
   reset: () => void
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
+  
+  const router = useRouter()
  
   return (
     <div>
       <h2>Something went wrong!</h2>
+      <h2>{`${error}`}</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          ()=>router.back()
         }
       >
         Try again
