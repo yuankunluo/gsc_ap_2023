065e70de69168563515928c12a016f4c0c1e740a
yuankunluo <yuankunluo@outlook.com>
2023-06-21 00:27:38 +0800

    simplfy front-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,83 +2,52 @@ import Link from "next/link";
 
 export default function Home() {
 
+  const menus = [
+    {
+      title: "我的坐席",
+      supTitle: "My Seat.",
+      route: "/seat"
+    },
+    {
+      title: "签到",
+      supTitle: "Check In.",
+      route: "/check-in"
+    },
+    {
+      title: "转让坐席",
+      supTitle: "Transfer.",
+      route: "/switch"
+    },
+    {
+      title: "节目单",
+      supTitle: "Playlist.",
+      route: "/menu"
+    },
+    {
+      title: "管理",
+      supTitle: "Admin.",
+      route: "/admin"
+    }
+  ]
+  
+
   return (
       <div>
-
-
-      <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
-          <Link href="/seat">
+        {menus.map(menu => {
+          return <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
+          <Link href={menu.route}>
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              我的坐席{' '}
+              {menu.title}{' '}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              My Seat.
+              {menu.supTitle}
             </p>
           </Link>
         </div>
-
-        <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
-          <Link href="/check-in">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              签到{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Check In.
-            </p>
-          </Link>
-        </div>
-
-
-        <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
-          <Link href="/switch">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              转让坐席{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Transfer.
-            </p>
-          </Link>
-        </div>
-
-        <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
-          <Link href="/menu">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              节目单{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Playlist.
-            </p>
-          </Link>
-        </div>
-
-        <div className="group w-64 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:text-fuchsia-600 hover:bg-[url('/girl.png')]">
-          <Link href="/admin">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              管理{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Admin.
-            </p>
-          </Link>
-        </div>
-
-
-
-        </div>
+        })}
+      </div>
   )
 }
